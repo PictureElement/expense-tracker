b0d44e41d5ea8f95d179f17acae3cdb35c138dac
PictureElement <marios.sofokleous@yandex.com>
2021-11-13 20:44:31 +0200

    Setup MUI. Add Basic App Bar.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>
-      Hello World
+      {/* Basic App Bar */}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Expense Tracker
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
     </div>
   );
 }
