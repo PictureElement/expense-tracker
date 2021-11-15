059cc1a697915f9049912a13e9db3188a441e33e
PictureElement <marios.sofokleous@yandex.com>
2021-11-15 21:39:08 +0200

    Calculate income

--- src/components/Income.js
+++ src/components/Income.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
+import { Context } from '../context/Store';
 
 function Income() {
+  const [state, setState] = useContext(Context);
+
+  const incomeItems = state.transactions.filter(item => item.type === 'income');
+
+  const incomeAmounts = incomeItems.map(({amount}) => amount);
+
+  const income = incomeAmounts.reduce(((previousAmount, currentAmount) => previousAmount + currentAmount), 0);
+
   return (
     <Card
       sx={{
@@ -11,7 +20,7 @@ function Income() {
       }}
     >
       <Box sx={{ color: 'text.secondary', typography: 'overline' }}>INCOME</Box>
-      <Box sx={{ color: 'success.main', typography: 'h5' }}>$511.00</Box>
+      <Box sx={{ color: 'success.main', typography: 'h5' }}>+ € {income}</Box>
     </Card>
   )
 }
